d8cdd13bf1d44cd115e5d0e37ed1063314db5a68
Stefan Engels <stefan.engels@tum.de>
2023-03-02 09:48:40 +0100

    add xml test preliminary

--- test/test_railwaynetwork.cpp
+++ test/test_railwaynetwork.cpp
@@ -1,7 +1,19 @@
 #include "RailwayNetwork.hpp"
 #include "gtest/gtest.h"
+#include "xml/tinyxml2.h"
+#include <iostream>
+#include <filesystem>
 
 TEST(Functionaliy, SimpleTest) {
-    RailwayNetwork g;
+    cda_rail::Network g;
     EXPECT_TRUE(g.edges.size() == 0); // dummy test
+}
+
+TEST(Functionality, XML) {
+    tinyxml2::XMLDocument graph_xml;
+    graph_xml.LoadFile("../../example-networks/Fig11/network/tracks.gexf");
+    std::cout << "Current path is "  << std::filesystem::current_path() << std::endl;
+    std::cout << "Try loading ../../example-networks/Fig11/network/tracks.gexf" << std::endl;
+    std::cout << "XML loading message: " << ((graph_xml.ErrorID() == 0) ? "OK" : graph_xml.ErrorStr()) << std::endl;
+    EXPECT_TRUE(graph_xml.ErrorID() == 0);
 }
