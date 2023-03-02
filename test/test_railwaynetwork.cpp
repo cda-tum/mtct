dc24b5020120d18d5c8cb3ee8b0e5bfe17c45b57
Tom Peham <pehamtom@gmx.at>
2023-03-02 13:36:04 +0100

    Moved testfiles to test directory.

--- test/test_railwaynetwork.cpp
+++ test/test_railwaynetwork.cpp
@@ -11,9 +11,9 @@ TEST(Functionaliy, SimpleTest) {
 
 TEST(Functionality, XML) {
     tinyxml2::XMLDocument graph_xml;
-    graph_xml.LoadFile("../../example-networks/Fig11/network/tracks.gexf");
+    graph_xml.LoadFile("./example-networks/Fig11/network/tracks.gexf");
     std::cout << "Current path is "  << std::filesystem::current_path() << std::endl;
-    std::cout << "Try loading ../../example-networks/Fig11/network/tracks.gexf" << std::endl;
+    std::cout << "Try loading example-networks/Fig11/network/tracks.gexf" << std::endl;
     std::cout << "XML loading message: " << ((graph_xml.ErrorID() == 0) ? "OK" : graph_xml.ErrorStr()) << std::endl;
     EXPECT_TRUE(graph_xml.ErrorID() == 0);
 }
