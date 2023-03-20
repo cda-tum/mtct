b7d592cb585b32ea5468679a06d0eb4403e8acf0
Stefan Engels <stefan.engels@tum.de>
2023-03-20 17:11:31 +0100

    test importing xml and json

--- test/test_railwaynetwork.cpp
+++ test/test_railwaynetwork.cpp
@@ -1,216 +1,182 @@
 #include "RailwayNetwork.hpp"
 #include "gtest/gtest.h"
-#include "parsing/tinyxml2.h"
-#include <iostream>
-#include <filesystem>
-#include <unordered_map>
 #include <algorithm>
-#include <cctype>
 #include "parsing/json.hpp"
-#include <fstream>
+#include <unordered_set>
 
 using json = nlohmann::json;
 
-TEST(Functionaliy, SimpleTest) {
-    cda_rail::Network g;
-    EXPECT_TRUE(g.edges.size() == 0); // dummy test
+struct EdgeTarget {
+    std::string source;
+    std::string target;
+    double length;
+    double max_speed;
+    bool breakable;
+    double min_block_length;
+};
+
+TEST(Functionality, ReadNetwork) {
+    cda_rail::Network network = cda_rail::Network::read_network("./example-networks/Fig11/network/");
+
+    // Check vertices properties
+    std::vector<std::string> vertex_names = {"l0", "l1", "l2", "l3", "r0", "r1", "r2", "g00", "g01", "g10", "g11"};
+    std::vector<int> type = {2,2,2,0,2,2,0,2,2,2,2};
+
+    EXPECT_TRUE(network.number_of_vertices() == vertex_names.size());
+
+    for (int i = 0; i < vertex_names.size(); i++) {
+        std::string v_name = vertex_names[i];
+        cda_rail::Vertex v = network.get_vertex(v_name);
+        EXPECT_TRUE(v.name == v_name);
+        EXPECT_TRUE(v.type == type[i]);
+    }
+
+    // Check edges properties
+    std::vector<EdgeTarget> edge_targets;
+    edge_targets.push_back({"l0", "l1", 500, 27.77777777777778, true, 0});
+    edge_targets.push_back({"l1", "l2", 500, 27.77777777777778, true, 0});
+    edge_targets.push_back({"l2", "l3", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"l3", "g00", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"l3", "g10", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"g00", "g01", 300, 27.77777777777778, true, 0});
+    edge_targets.push_back({"g10", "g11", 300, 27.77777777777778, true, 0});
+    edge_targets.push_back({"g01", "r2", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"g11", "r2", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"r2", "r1", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"r1", "r0", 500, 27.77777777777778, true, 0});
+    edge_targets.push_back({"r0", "r1", 500, 27.77777777777778, true, 0});
+    edge_targets.push_back({"r1", "r2", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"r2", "g01", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"r2", "g11", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"g01", "g00", 300, 27.77777777777778, true, 0});
+    edge_targets.push_back({"g11", "g10", 300, 27.77777777777778, true, 0});
+    edge_targets.push_back({"g00", "l3", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"g10", "l3", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"l3", "l2", 5, 27.77777777777778, false, 0});
+    edge_targets.push_back({"l2", "l1", 500, 27.77777777777778, true, 0});
+    edge_targets.push_back({"l1", "l0", 500, 27.77777777777778, true, 0});
+
+    EXPECT_TRUE(network.number_of_edges() == edge_targets.size());
+    for (const auto& edge : edge_targets) {
+        cda_rail::Edge e = network.get_edge(edge.source, edge.target);
+        EXPECT_TRUE(network.get_vertex(e.source).name == edge.source);
+        EXPECT_TRUE(network.get_vertex(e.target).name == edge.target);
+        EXPECT_TRUE(e.length == edge.length);
+        EXPECT_TRUE(e.max_speed == edge.max_speed);
+        EXPECT_TRUE(e.breakable == edge.breakable);
+        EXPECT_TRUE(e.min_block_length == edge.min_block_length);
+    }
+
+    // Check successors
+    std::unordered_set<int> successors_target;
+
+    // l0,l1
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l1", "l2"));
+    EXPECT_TRUE(network.get_successors("l0", "l1") == successors_target);
+
+    // l1,l2
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l2", "l3"));
+    EXPECT_TRUE(network.get_successors("l1", "l2") == successors_target);
+
+    // l2,l3
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l3", "g00"));
+    successors_target.insert(network.get_edge_index("l3", "g10"));
+    EXPECT_TRUE(network.get_successors("l2", "l3") == successors_target);
+
+    // l3,g00
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g00", "g01"));
+    EXPECT_TRUE(network.get_successors("l3", "g00") == successors_target);
+
+    // l3,g10
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g10", "g11"));
+    EXPECT_TRUE(network.get_successors("l3", "g10") == successors_target);
+
+    // g00,g01
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g01", "r2"));
+    EXPECT_TRUE(network.get_successors("g00", "g01") == successors_target);
+
+    // g10,g11
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g11", "r2"));
+    EXPECT_TRUE(network.get_successors("g10", "g11") == successors_target);
+
+    // g01,r2
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("r2", "r1"));
+    EXPECT_TRUE(network.get_successors("g01", "r2") == successors_target);
+
+    // g11,r2
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("r2", "r1"));
+    EXPECT_TRUE(network.get_successors("g11", "r2") == successors_target);
+
+    // r2,r1
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("r1", "r0"));
+    EXPECT_TRUE(network.get_successors("r2", "r1") == successors_target);
+
+    // r1,r0
+    successors_target.clear();
+    EXPECT_TRUE(network.get_successors("r1", "r0") == successors_target);
+
+    // r0,r1
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("r1", "r2"));
+    EXPECT_TRUE(network.get_successors("r0", "r1") == successors_target);
+
+    // r1,r2
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("r2", "g01"));
+    successors_target.insert(network.get_edge_index("r2", "g11"));
+    EXPECT_TRUE(network.get_successors("r1", "r2") == successors_target);
+
+    // r2,g01
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g01", "g00"));
+    EXPECT_TRUE(network.get_successors("r2", "g01") == successors_target);
+
+    // r2,g11
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g11", "g10"));
+    EXPECT_TRUE(network.get_successors("r2", "g11") == successors_target);
+
+    // g01,g00
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g00", "l3"));
+    EXPECT_TRUE(network.get_successors("g01", "g00") == successors_target);
+
+    // g11,g10
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("g10", "l3"));
+    EXPECT_TRUE(network.get_successors("g11", "g10") == successors_target);
+
+    // g00,l3
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l3", "l2"));
+    EXPECT_TRUE(network.get_successors("g00", "l3") == successors_target);
+
+    // g10,l3
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l3", "l2"));
+    EXPECT_TRUE(network.get_successors("g10", "l3") == successors_target);
+
+    // l3,l2
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l2", "l1"));
+    EXPECT_TRUE(network.get_successors("l3", "l2") == successors_target);
+
+    // l2,l1
+    successors_target.clear();
+    successors_target.insert(network.get_edge_index("l1", "l0"));
+    EXPECT_TRUE(network.get_successors("l2", "l1") == successors_target);
+
+    // l1,l0
+    successors_target.clear();
+    EXPECT_TRUE(network.get_successors("l1", "l0") == successors_target);
 }
-
-/**
-TEST(Functionality, ReadXML) {
-    tinyxml2::XMLDocument graph_xml;
-    graph_xml.LoadFile("./example-networks/Fig11/network/tracks.graphml");
-    std::cout << "Current path is "  << std::filesystem::current_path() << std::endl;
-    std::cout << "Try loading example-networks/Fig11/network/tracks.graphml" << std::endl;
-    std::cout << "XML loading message: " << ((graph_xml.ErrorID() == 0) ? "OK" : graph_xml.ErrorStr()) << std::endl;
-    EXPECT_TRUE(graph_xml.ErrorID() == 0);
-}
-
-TEST(Functionality, ParseXML) {
-    tinyxml2::XMLDocument graph_xml;
-    graph_xml.LoadFile("./example-networks/Fig11/network/tracks.graphml");
-    EXPECT_TRUE(graph_xml.ErrorID() == 0);
-
-    tinyxml2::XMLElement* graphml_body = graph_xml.FirstChildElement("graphml");
-
-    tinyxml2::XMLElement* graphml_key = graphml_body->FirstChildElement("key");
-    std::string breakable, min_block_length, max_speed, length, type;
-    while (graphml_key) {
-        if (graphml_key->Attribute("attr.name") == std::string("breakable")) {
-            breakable = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("min_block_length")) {
-            min_block_length = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("max_speed")) {
-            max_speed = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("length")) {
-            length = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("type")) {
-            type = graphml_key->Attribute("id");
-        }
-        graphml_key = graphml_key->NextSiblingElement("key");
-    }
-    std::cout << "breakable: " << breakable << std::endl
-        << "min_block_length: " << min_block_length << std::endl
-        << "max_speed: " << max_speed << std::endl
-        << "length: " << length << std::endl
-        << "type: " << type << std::endl;
-
-    tinyxml2::XMLElement* graphml_graph = graphml_body->FirstChildElement("graph");
-    std::cout << "Graph type: " << graphml_graph->Attribute("edgedefault") << std::endl;
-    EXPECT_TRUE((graphml_graph->Attribute("edgedefault")) == std::string("directed"));
-
-    cda_rail::Network nx = cda_rail::Network();
-    std::unordered_map<std::string, int> index_map = std::unordered_map<std::string, int>();
-
-    tinyxml2::XMLElement* graphml_node = graphml_graph->FirstChildElement("node");
-    while (graphml_node) {
-        cda_rail::Vertex v = cda_rail::Vertex();
-        v.name = graphml_node->Attribute("id");
-        tinyxml2::XMLElement* graphml_data = graphml_node->FirstChildElement("data");
-        while (graphml_data) {
-            if (graphml_data->Attribute("key") == type) {
-                v.type = std::stoi(graphml_data->GetText());
-            }
-            graphml_data = graphml_data->NextSiblingElement("data");
-        }
-        nx.vertices.push_back(v);
-        index_map[v.name] = nx.vertices.size() - 1;
-        std::cout << "Vector has " << nx.vertices.size() << " elements and a capacity of " << nx.vertices.capacity() << std::endl;
-        graphml_node = graphml_node->NextSiblingElement("node");
-    }
-
-    for (int i = 0; i < nx.vertices.size(); ++i) {
-        std::cout << "Vertex " << i << ": " << nx.vertices[i].name << " mapped to " << index_map[nx.vertices[i].name] << std::endl;
-        EXPECT_TRUE(i == index_map[nx.vertices[i].name]);
-    }
-
-    tinyxml2::XMLElement* graphml_edge = graphml_graph->FirstChildElement("edge");
-    while (graphml_edge) {
-        cda_rail::Edge e = cda_rail::Edge();
-        e.source = &nx.vertices[index_map[graphml_edge->Attribute("source")]];
-        e.target = &nx.vertices[index_map[graphml_edge->Attribute("target")]];
-
-        tinyxml2::XMLElement* graphml_data = graphml_edge->FirstChildElement("data");
-        while (graphml_data) {
-            if (graphml_data->Attribute("key") == length) {
-                e.length = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == max_speed) {
-                e.max_speed = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == min_block_length) {
-                e.min_block_length = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == breakable) {
-                std::string breakable_str = graphml_data->GetText();
-                std::transform(breakable_str.begin(), breakable_str.end(), breakable_str.begin(), ::tolower);
-                std:std::istringstream is(breakable_str);
-                is >> std::boolalpha >> e.breakable;
-            }
-            graphml_data = graphml_data->NextSiblingElement("data");
-        }
-        nx.edges.push_back(e);
-        graphml_edge = graphml_edge->NextSiblingElement("edge");
-    }
-}
-
-TEST(Functionality, JSON) {
-    tinyxml2::XMLDocument graph_xml;
-    graph_xml.LoadFile("./example-networks/Fig11/network/tracks.graphml");
-    EXPECT_TRUE(graph_xml.ErrorID() == 0);
-
-    tinyxml2::XMLElement* graphml_body = graph_xml.FirstChildElement("graphml");
-
-    tinyxml2::XMLElement* graphml_key = graphml_body->FirstChildElement("key");
-    std::string breakable, min_block_length, max_speed, length, type;
-    while (graphml_key) {
-        if (graphml_key->Attribute("attr.name") == std::string("breakable")) {
-            breakable = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("min_block_length")) {
-            min_block_length = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("max_speed")) {
-            max_speed = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("length")) {
-            length = graphml_key->Attribute("id");
-        }
-        else if (graphml_key->Attribute("attr.name") == std::string("type")) {
-            type = graphml_key->Attribute("id");
-        }
-        graphml_key = graphml_key->NextSiblingElement("key");
-    }
-
-    tinyxml2::XMLElement* graphml_graph = graphml_body->FirstChildElement("graph");
-    EXPECT_TRUE((graphml_graph->Attribute("edgedefault")) == std::string("directed"));
-
-    cda_rail::Network nx = cda_rail::Network();
-    std::unordered_map<std::string, int> vertex_index_map = std::unordered_map<std::string, int>();
-
-    tinyxml2::XMLElement* graphml_node = graphml_graph->FirstChildElement("node");
-    while (graphml_node) {
-        cda_rail::Vertex v = cda_rail::Vertex();
-        v.name = graphml_node->Attribute("id");
-        tinyxml2::XMLElement* graphml_data = graphml_node->FirstChildElement("data");
-        while (graphml_data) {
-            if (graphml_data->Attribute("key") == type) {
-                v.type = std::stoi(graphml_data->GetText());
-            }
-            graphml_data = graphml_data->NextSiblingElement("data");
-        }
-        nx.vertices.push_back(v);
-        vertex_index_map[v.name] = nx.vertices.size() - 1;
-        graphml_node = graphml_node->NextSiblingElement("node");
-    }
-
-    for (int i = 0; i < nx.vertices.size(); ++i) {
-        EXPECT_TRUE(i == vertex_index_map[nx.vertices[i].name]);
-    }
-
-    std::unordered_map<std::string, int> edge_index_map = std::unordered_map<std::string, int>();
-    tinyxml2::XMLElement* graphml_edge = graphml_graph->FirstChildElement("edge");
-    while (graphml_edge) {
-        cda_rail::Edge e = cda_rail::Edge();
-        e.source = &nx.vertices[vertex_index_map[graphml_edge->Attribute("source")]];
-        e.target = &nx.vertices[vertex_index_map[graphml_edge->Attribute("target")]];
-
-        tinyxml2::XMLElement* graphml_data = graphml_edge->FirstChildElement("data");
-        while (graphml_data) {
-            if (graphml_data->Attribute("key") == length) {
-                e.length = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == max_speed) {
-                e.max_speed = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == min_block_length) {
-                e.min_block_length = std::stod(graphml_data->GetText());
-            }
-            else if (graphml_data->Attribute("key") == breakable) {
-                std::string breakable_str = graphml_data->GetText();
-                std::transform(breakable_str.begin(), breakable_str.end(), breakable_str.begin(), ::tolower);
-                std:std::istringstream is(breakable_str);
-                is >> std::boolalpha >> e.breakable;
-            }
-            graphml_data = graphml_data->NextSiblingElement("data");
-        }
-        nx.edges.push_back(e);
-        edge_index_map[e.toString()] = nx.edges.size() - 1;
-        graphml_edge = graphml_edge->NextSiblingElement("edge");
-    }
-
-    std::ifstream f("./example-networks/Fig11/network/successors_cpp_2.json");
-    json data = json::parse(f);
-    for (auto& [key, val] : data.items()) {
-        std::cout << "key: " << key << ", value: " << val << " of size " << val.size() << std::endl;
-        for (auto& tuple : val) {
-            std::string edge_str = "('" + tuple[0].get<std::string>() + "', '" + tuple[1].get<std::string>() + "')";
-            std::cout << "edge_str: " << edge_str << std::endl;
-        }
-    }
-}
- **/
