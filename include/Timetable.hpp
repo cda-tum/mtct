85fbe31357c6859d65e49523faeeaa035ec4f409
Stefan Engels <stefan.engels@tum.de>
2023-03-22 16:00:04 +0100

    begin of timetable

--- include/Timetable.hpp
+++ include/Timetable.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
+#include "Train.hpp"
 
 namespace cda_rail {
     struct Station {
@@ -10,6 +12,25 @@ namespace cda_rail {
          * @param tracks Unordered set of edges that define the station.
          */
         std::string name;
-        std::unordered_set<int> tracks;
+        std::unordered_set<int> tracks = {};
+    };
+
+    struct Interval {
+        /**
+         * Interval struct in seconds.
+         */
+        int begin;
+        int end;
+    };
+
+    class Timetable {
+        /**
+         * Timetable class
+         */
+        private:
+            std::vector<Station> stations;
+            std::vector<Train> trains;
+            std::unordered_map<std::string, int> station_name_to_index;
+            std::unordered_map<std::string, int> train_name_to_index;
     };
 }
