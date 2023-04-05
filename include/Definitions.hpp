e148edfafd0be023b00078cc96dddb480c099485
Stefan Engels <stefan.engels@tum.de>
2023-04-05 10:37:42 +0200

    add basic function to check if a path is a directory in principle and create it if it does not exist but is a directory

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -1,8 +1,28 @@
 #pragma once
+#include <filesystem>
 
 namespace cda_rail {
     // Constants for vertex type
     static const int NO_BORDER = 0;
     static const int VSS = 1;
     static const int TTD = 2;
+
+    bool is_directory_and_create(const std::filesystem::path& p) {
+        /**
+         * Checks if a directory exists and creates it if it doesn't.
+         * Returns true if the directory exists or was created successfully.
+         * Returns false if the directory could not be created, probably because the path is not a directory.
+         *
+         * @param p Path to the directory
+         */
+
+        if (!std::filesystem::exists(p)) {
+            std::error_code error_code;
+            std::filesystem::create_directory(p, error_code);
+            if (error_code) {
+                return false;
+            }
+        }
+        return std::filesystem::is_directory(p);
+    };
 }
