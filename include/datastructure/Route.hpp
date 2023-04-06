a9eddc45ae6923dc41ffe746229aab87885f595a
Stefan Engels <stefan.engels@tum.de>
2023-04-06 15:47:15 +0200

    add push back to route

--- include/datastructure/Route.hpp
+++ include/datastructure/Route.hpp
@@ -10,23 +10,23 @@ namespace cda_rail {
             std::vector<int> edges;
         public:
             void push_back_edge(int edge_index, cda_rail::Network network);
-            void push_back_edge(int source, int target, cda_rail::Network);
-            void push_back_edge(const std::string& source, const std::string& target, cda_rail::Network);
+            template<typename T>
+            void push_back_edge(T source, T target, cda_rail::Network network);
 
             void push_front_edge(int edge_index, cda_rail::Network network);
-            void push_front_edge(int source, int target, cda_rail::Network);
-            void push_front_edge(const std::string& source, const std::string& target, cda_rail::Network);
+            template<typename T>
+            void push_front_edge(T source, T target, cda_rail::Network network);
 
             void remove_edge(int edge_index, cda_rail::Network network);
-            void remove_edge(int source, int target, cda_rail::Network);
-            void remove_edge(const std::string& source, const std::string& target, cda_rail::Network);
+            template<typename T>
+            void remove_edge(T source, T target, cda_rail::Network network);
 
-            void export_route(const std::filesystem::path& p, cda_rail::Network) const;
-            void export_route(const std::string& path, cda_rail::Network) const;
-            void export_route(const char* path, cda_rail::Network) const;
+            void export_route(const std::filesystem::path& p, cda_rail::Network network) const;
+            void export_route(const std::string& path, cda_rail::Network network) const;
+            void export_route(const char* path, cda_rail::Network network) const;
 
-            void import_route(const std::filesystem::path& p, cda_rail::Network);
-            void import_route(const std::string& path, cda_rail::Network);
-            void import_route(const char* path, cda_rail::Network);
+            void import_route(const std::filesystem::path& p, cda_rail::Network network);
+            void import_route(const std::string& path, cda_rail::Network network);
+            void import_route(const char* path, cda_rail::Network network);
     };
 }
--- src/datastructure/Route.cpp
+++ src/datastructure/Route.cpp
@@ -9,4 +9,32 @@ void cda_rail::Route::push_back_edge(int edge_index, cda_rail::Network network) 
      * @param edge_index The index of the edge to add.
      * @param network The network to which the edge belongs.
      */
+
+    if (!network.has_edge(edge_index)) {
+        throw std::out_of_range("Edge does not exist.");
+    }
+    if (edges.size() > 0 && !network.is_valid_successor(edges.back(), edge_index)) {
+        throw std::out_of_range("Edge is not a valid successor.");
+    }
+    edges.push_back(edge_index);
+}
+
+template<typename T>
+void cda_rail::Route::push_back_edge(T source, T target, cda_rail::Network network) {
+    /**
+     * Adds the edge to the end of the route.
+     * Throws an error if the edge does not exist in the network or is not a valid successor of the last edge.
+     *
+     * @param source The source of the edge to add.
+     * @param target The target of the edge to add.
+     * @param network The network to which the edge belongs.
+     */
+
+    if (!network.has_edge(source, target)) {
+        throw std::out_of_range("Edge does not exist.");
+    }
+    if (edges.size() > 0 && !network.is_valid_successor(edges.back(), std::pair<T, T>(source, target))) {
+        throw std::out_of_range("Edge is not a valid successor.");
+    }
+    edges.push_back(network.get_edge_index(source, target));
 }
