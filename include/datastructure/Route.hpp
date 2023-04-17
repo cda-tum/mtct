fb409a5e4d4a9d23d81ebe77d2ee8385d74b8677
Stefan Engels <stefan.engels@tum.de>
2023-04-17 16:34:38 +0200

    function headers for routes map

--- include/datastructure/Route.hpp
+++ include/datastructure/Route.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include "datastructure/RailwayNetwork.hpp"
+#include "datastructure/Train.hpp"
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <unordered_map>
 
 namespace cda_rail {
     class Route {
@@ -23,5 +25,36 @@ namespace cda_rail {
             [[nodiscard]] int get_edge(int route_index) const;
             [[nodiscard]] const cda_rail::Edge& get_edge(int route_index, cda_rail::Network network) const;
             [[nodiscard]] int size() const;
+
+            [[nodiscard]] bool assert(cda_rail::Network& network) const;
+    };
+
+    class RouteMap {
+        private:
+            std::unordered_map<std::string, Route> routes;
+        public:
+            void add_empty_route(const std::string& train_name);
+            void add_empty_route(const std::string& train_name, cda_rail::TrainList& trains);
+
+            template<typename... Ts>
+            void push_back_edge(const std::string& train_name, Ts&&... args);
+
+            template<typename... Ts>
+            void push_front_edge(const std::string& train_name, Ts&&... args);
+
+            void remove_first_edge(const std::string& train_name);
+            void remove_last_edge(const std::string& train_name);
+
+            const Route& get_route(const std::string& train_name) const;
+
+            [[nodiscard]] bool assert(cda_rail::TrainList& trains, cda_rail::Network& network) const;
+
+            void export_routes(const std::filesystem::path& p, cda_rail::Network& network) const;
+            void export_routes(const std::string& path, cda_rail::Network& network) const;
+            void export_routes(const char* path, cda_rail::Network& network) const;
+
+            [[nodiscard]] static RouteMap import_routes(const std::filesystem::path& p, cda_rail::Network& network);
+            [[nodiscard]] static RouteMap import_routes(const std::string& path, cda_rail::Network& network);
+            [[nodiscard]] static RouteMap import_routes(const char* path, cda_rail::Network& network);
     };
 }
