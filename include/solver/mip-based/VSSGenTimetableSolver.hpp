9ba7b299b0b8c10241fe43459509d52bf45a1774
Stefan Engels <stefan.engels@tum.de>
2023-04-24 11:44:58 +0200

    add constructors

--- include/solver/mip-based/VSSGenTimetableSolver.hpp
+++ include/solver/mip-based/VSSGenTimetableSolver.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+#include <filesystem>
+#include "probleminstances/VSSGenerationTimetable.hpp"
+
+namespace cda_rail::solver::mip_based {
+    class VSSGenTimetableSolver {
+        private:
+            cda_rail::instances::VSSGenerationTimetable instance;
+
+        public:
+            // Constructors
+            explicit VSSGenTimetableSolver(const cda_rail::instances::VSSGenerationTimetable& instance);
+            explicit VSSGenTimetableSolver(const std::filesystem::path& instance_path);
+            explicit VSSGenTimetableSolver(const std::string& instance_path);
+            explicit VSSGenTimetableSolver(const char* instance_path);
+    };
+}
--- src/solver/mip-based/VSSGenTimetableSolver.cpp
+++ src/solver/mip-based/VSSGenTimetableSolver.cpp
@@ -1,1 +1,16 @@
 #include "solver/mip-based/VSSGenTimetableSolver.hpp"
+
+cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(
+        const cda_rail::instances::VSSGenerationTimetable &instance) : instance(instance) {}
+
+cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(const std::filesystem::path &instance_path) {
+    instance = cda_rail::instances::VSSGenerationTimetable::import_instance(instance_path);
+}
+
+cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(const std::string &instance_path) {
+    instance = cda_rail::instances::VSSGenerationTimetable::import_instance(instance_path);
+}
+
+cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(const char *instance_path) {
+    instance = cda_rail::instances::VSSGenerationTimetable::import_instance(instance_path);
+}
