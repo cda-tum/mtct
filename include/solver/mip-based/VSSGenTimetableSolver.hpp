990d53e7ced5fe4cffc8f9edea2c059237ede759
Stefan Engels <stefan.engels@tum.de>
2023-04-24 14:46:57 +0200

    start with solve

--- include/solver/mip-based/VSSGenTimetableSolver.hpp
+++ include/solver/mip-based/VSSGenTimetableSolver.hpp
@@ -2,11 +2,18 @@
 #include <string>
 #include <filesystem>
 #include "probleminstances/VSSGenerationTimetable.hpp"
+#include "gurobi_c++.h"
+#include "unordered_map"
 
 namespace cda_rail::solver::mip_based {
     class VSSGenTimetableSolver {
+        // TODO: Maybe add an abstract parent class later on
         private:
             cda_rail::instances::VSSGenerationTimetable instance;
+
+            // Gurobi variables
+            GRBEnv env = nullptr;
+            GRBModel model = nullptr;
 
         public:
             // Constructors
@@ -14,5 +21,8 @@ namespace cda_rail::solver::mip_based {
             explicit VSSGenTimetableSolver(const std::filesystem::path& instance_path);
             explicit VSSGenTimetableSolver(const std::string& instance_path);
             explicit VSSGenTimetableSolver(const char* instance_path);
+
+            // Methods
+            void solve(); // to be added with parameters and return type later
     };
 }
--- src/solver/mip-based/VSSGenTimetableSolver.cpp
+++ src/solver/mip-based/VSSGenTimetableSolver.cpp
@@ -1,4 +1,5 @@
 #include "solver/mip-based/VSSGenTimetableSolver.hpp"
+#include "gurobi_c++.h"
 
 cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(
         const cda_rail::instances::VSSGenerationTimetable &instance) : instance(instance) {}
@@ -13,4 +14,15 @@ cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(const 
 
 cda_rail::solver::mip_based::VSSGenTimetableSolver::VSSGenTimetableSolver(const char *instance_path) {
     instance = cda_rail::instances::VSSGenerationTimetable::import_instance(instance_path);
+}
+
+void cda_rail::solver::mip_based::VSSGenTimetableSolver::solve() {
+    /**
+     * Solve the instance using Gurobi
+     */
+
+    // Create environment and model
+    GRBEnv env(true);
+    env.start();
+    GRBModel model(env);
 }
