a7ea2e3f180882c069fe5c8ead172a7b576d654b
Stefan Engels <stefan.engels@tum.de>
2023-04-26 18:01:53 +0200

    start creating variables

--- src/solver/mip-based/VSSGenTimetableSolver.cpp
+++ src/solver/mip-based/VSSGenTimetableSolver.cpp
@@ -40,6 +40,17 @@ void cda_rail::solver::mip_based::VSSGenTimetableSolver::create_fixed_routes_var
      * Creates variables connected to the fixed route version of the problem
      */
 
+    // Create MultiArrays
     vars["lda"] = MultiArray<GRBVar>(num_tr, num_t);
     vars["mu"] = MultiArray<GRBVar>(num_tr, num_t);
+
+    auto train_list = instance.get_train_list();
+    for (int i = 0; i < num_tr; ++i) {
+        auto tr_name = train_list.get_train(i).name;
+        auto r_len = instance.route_length(tr_name);
+        for (int t_steps = 0; t_steps < num_t; ++t_steps) {
+            auto t = t_steps * dt;
+            vars["lda"](i, t_steps) = model.addVar(0, r_len, 0, GRB_CONTINUOUS, "lda_" + tr_name + "_" + std::to_string(t));
+        }
+    }
 }
