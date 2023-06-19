7426aebb951fe9400b17357a5955b3f5ecae025c
Stefan Engels <stefan.engels@tum.de>
2023-06-19 15:51:40 +0200

    another cmath import missing

--- src/solver/mip-based/VSSGenTimetableSolver_freeRoutes.cpp
+++ src/solver/mip-based/VSSGenTimetableSolver_freeRoutes.cpp
@@ -2,6 +2,7 @@
 #include "gurobi_c++.h"
 #include "MultiArray.hpp"
 #include <exception>
+#include <cmath>
 
 void cda_rail::solver::mip_based::VSSGenTimetableSolver::create_free_routes_variables() {
     /**
