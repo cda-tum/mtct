7b802eb61d5c12e5ec2bc48896914b13dc4ec276
Stefan Engels <stefan.engels@tum.de>
2023-07-12 08:02:22 +0200

    temporarily add debug output to test

--- test/test_gurobi_vss_gen.cpp
+++ test/test_gurobi_vss_gen.cpp
@@ -6,6 +6,6 @@
 TEST(Solver, GurobiVSSGenFixedRoute) {
     cda_rail::solver::mip_based::VSSGenTimetableSolver solver("./example-networks/SimpleStation/");
 
-   auto obj_val = solver.solve();
+   auto obj_val = solver.solve(15, true, false, true, true, false, true, -1, true);
    EXPECT_EQ(obj_val, 1);
 }
