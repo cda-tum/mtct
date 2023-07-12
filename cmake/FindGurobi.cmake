0749a5be44e72b157b4ca6f7459389fe2c1c1e78
Stefan Engels <stefan.engels@tum.de>
2023-07-12 15:35:15 +0200

    Revert "TO REVERT more debug"
    This reverts commit 18b196341c673f3d5d604c8a3323460048ff4cb7.

--- cmake/FindGurobi.cmake
+++ cmake/FindGurobi.cmake
@@ -21,10 +21,9 @@ if (WIN32)
             ${GUROBI_BIN_DIR}/gurobi*.dll
             )
 else()
-    message("I am in ELSE")
     file(GLOB GUROBI_LIBRARY_LIST
             RELATIVE ${GUROBI_LIB_DIR}
-            ${GUROBI_LIB_DIR}/libgurobi*.dylib
+            ${GUROBI_LIB_DIR}/libgurobi*.(so/dylib)
             )
 endif()
 
