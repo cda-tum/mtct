415f5400e25c92a8a366165eee6a3a606ec2b6d9
Stefan Engels <stefan.engels@tum.de>
2023-07-13 14:49:13 +0200

    clang on app

--- apps/vss_generation_timetable_mip_testing.cpp
+++ apps/vss_generation_timetable_mip_testing.cpp
@@ -13,14 +13,14 @@ int main(int argc, char** argv) {
   std::cout << "Instance " << model_name << " loaded at " << instance_path
             << std::endl;
 
-  int  delta_t                  = std::stoi(argv[3]);
-  bool fix_routes               = std::stoi(argv[4]) != 0;
-  bool discretize_vss_positions = std::stoi(argv[5]) != 0;
-  bool include_train_dynamics   = std::stoi(argv[6]) != 0;
-  bool include_braking_curves   = std::stoi(argv[7]) != 0;
-  bool use_pwl                  = std::stoi(argv[8]) != 0;
-  bool use_schedule_cuts        = std::stoi(argv[9]) != 0;
-  int  timeout                  = std::stoi(argv[10]);
+  const int  delta_t                  = std::stoi(argv[3]);
+  const bool fix_routes               = std::stoi(argv[4]) != 0;
+  const bool discretize_vss_positions = std::stoi(argv[5]) != 0;
+  const bool include_train_dynamics   = std::stoi(argv[6]) != 0;
+  const bool include_braking_curves   = std::stoi(argv[7]) != 0;
+  const bool use_pwl                  = std::stoi(argv[8]) != 0;
+  const bool use_schedule_cuts        = std::stoi(argv[9]) != 0;
+  const int  timeout                  = std::stoi(argv[10]);
 
   std::cout << "The following parameters were passed to the toolkit:"
             << std::endl;
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
   }
   std::cout << "   timeout: " << timeout << "s" << std::endl;
 
-  std::string file_name =
+  const std::string file_name =
       model_name + "_" + std::to_string(delta_t) + "_" +
       std::to_string(static_cast<int>(fix_routes)) + "_" +
       std::to_string(static_cast<int>(discretize_vss_positions)) + "_" +
