31435a8cca345263a7cd9df0b072f25837e05eb6
Stefan Engels <stefan.engels@tum.de>
2023-07-13 14:51:22 +0200

    update test case to default

--- test/test_gurobi_vss_gen.cpp
+++ test/test_gurobi_vss_gen.cpp
@@ -7,7 +7,6 @@ TEST(Solver, GurobiVSSGenFixedRoute) {
   cda_rail::solver::mip_based::VSSGenTimetableSolver solver(
       "./example-networks/SimpleStation/");
 
-  auto obj_val =
-      solver.solve(15, true, false, true, true, false, true, -1, true);
+  auto obj_val = solver.solve();
   EXPECT_EQ(obj_val, 1);
 }
