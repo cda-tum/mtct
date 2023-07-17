9675ef37c22aa21b58a325a05611f063ab588336
Stefan Engels <stefan.engels@tum.de>
2023-07-17 10:29:39 +0200

    remove pointer arithmetic due to gsl span usage.

--- apps/vss_generation_timetable_mip_testing.cpp
+++ apps/vss_generation_timetable_mip_testing.cpp
@@ -1,26 +1,30 @@
 #include "solver/mip-based/VSSGenTimetableSolver.hpp"
 
+#include <gsl/span>
+
 int main(int argc, char** argv) {
   if (argc != 11) {
     std::cout << "Expected 10 arguments, got " << argc - 1 << std::endl;
     std::exit(-1);
   }
 
-  const std::string                                  model_name    = argv[1];
-  const std::string                                  instance_path = argv[2];
+  auto args = gsl::span<char*>(argv, argc);
+
+  const std::string                                  model_name    = args[1];
+  const std::string                                  instance_path = args[2];
   cda_rail::solver::mip_based::VSSGenTimetableSolver solver(instance_path);
 
   std::cout << "Instance " << model_name << " loaded at " << instance_path
             << std::endl;
 
-  const int  delta_t                  = std::stoi(argv[3]);
-  const bool fix_routes               = std::stoi(argv[4]) != 0;
-  const bool discretize_vss_positions = std::stoi(argv[5]) != 0;
-  const bool include_train_dynamics   = std::stoi(argv[6]) != 0;
-  const bool include_braking_curves   = std::stoi(argv[7]) != 0;
-  const bool use_pwl                  = std::stoi(argv[8]) != 0;
-  const bool use_schedule_cuts        = std::stoi(argv[9]) != 0;
-  const int  timeout                  = std::stoi(argv[10]);
+  const int  delta_t                  = std::stoi(args[3]);
+  const bool fix_routes               = std::stoi(args[4]) != 0;
+  const bool discretize_vss_positions = std::stoi(args[5]) != 0;
+  const bool include_train_dynamics   = std::stoi(args[6]) != 0;
+  const bool include_braking_curves   = std::stoi(args[7]) != 0;
+  const bool use_pwl                  = std::stoi(args[8]) != 0;
+  const bool use_schedule_cuts        = std::stoi(args[9]) != 0;
+  const int  timeout                  = std::stoi(args[10]);
 
   std::cout << "The following parameters were passed to the toolkit:"
             << std::endl;
