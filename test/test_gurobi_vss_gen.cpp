fe583add563d956afdb36f97b2e593ffcc0803e0
Stefan Engels <stefan.engels@tum.de>
2023-08-02 16:03:36 +0200

    add failing test case

--- test/test_gurobi_vss_gen.cpp
+++ test/test_gurobi_vss_gen.cpp
@@ -3,6 +3,33 @@
 #include "gtest/gtest.h"
 #include <string>
 
+TEST(Solver, GurobiVSSGenDeltaTDefault) {
+  cda_rail::solver::mip_based::VSSGenTimetableSolver solver(
+      "./example-networks/SimpleStation/");
+
+  std::cout << "--------------------- TEST 1 ---------------------------"
+            << std::endl;
+  auto obj_val_6 = solver.solve(6);
+  std::cout << "--------------------- TEST 2 ---------------------------"
+            << std::endl;
+  auto obj_val_15 = solver.solve(15);
+  std::cout << "--------------------- TEST 3 ---------------------------"
+            << std::endl;
+  auto obj_val_11 = solver.solve(11);
+  std::cout << "--------------------- TEST 4 ---------------------------"
+            << std::endl;
+  auto obj_val_18 = solver.solve(18);
+  std::cout << "--------------------- TEST 5 ---------------------------"
+            << std::endl;
+  auto obj_val_30 = solver.solve(30);
+
+  EXPECT_EQ(obj_val_6, 1);
+  EXPECT_EQ(obj_val_15, 1);
+  EXPECT_EQ(obj_val_11, 1);
+  EXPECT_EQ(obj_val_18, 1);
+  EXPECT_EQ(obj_val_30, 1);
+}
+
 TEST(Solver, GurobiVSSGenFixedRoute) {
   cda_rail::solver::mip_based::VSSGenTimetableSolver solver(
       "./example-networks/SimpleStation/");
