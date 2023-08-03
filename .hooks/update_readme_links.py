7d6872587f0bdbb0846563e33f19f0d660889996
Stefan Engels <stefan.engels@tum.de>
2023-08-03 09:21:31 +0200

    finalize pre-commit

--- .hooks/update_readme_links.py
+++ .hooks/update_readme_links.py
@@ -7,8 +7,29 @@ print("Current branch:", current_branch)
 
 
 ubuntu = f"[![Ubuntu C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/ubuntu.yml?label=Ubuntu&logo=ubuntu&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/ubuntu.yml)\n"
+macos = f"[![macOS C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/macos.yml?label=macOS&logo=apple&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/macos.yml)\n"
+windows = f"[![Windows C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/windows.yml?label=Windows&logo=windows&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/windows.yml)\n"
+codeql = f"[![CodeQL](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/codeql-analysis.yml?label=CodeQL&logo=github&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/codeql-analysis.yml)\n"
+codecov = f"[![codecov](https://img.shields.io/codecov/c/github/cda-tum/mtct?label=Coverage&logo=codecov&style=flat-square?branch={current_branch})](https://codecov.io/gh/cda-tum/mtct)\n"
+
 
 lines = open("README.md").readlines()
-for line in lines:
-    if ubuntu[:20] in line:
-        print(f"Replacing {line} with {ubuntu}")
+with open("README.md", "w") as f:
+    for line in lines:
+        if ubuntu[:20] in line:
+            print(f"Replacing {line} with {ubuntu}")
+            f.write(ubuntu)
+        elif macos[:20] in line:
+            print(f"Replacing {line} with {macos}")
+            f.write(macos)
+        elif windows[:20] in line:
+            print(f"Replacing {line} with {windows}")
+            f.write(windows)
+        elif codeql[:20] in line:
+            print(f"Replacing {line} with {codeql}")
+            f.write(codeql)
+        elif codecov[:20] in line:
+            print(f"Replacing {line} with {codecov}")
+            f.write(codecov)
+        else:
+            f.write(line)
