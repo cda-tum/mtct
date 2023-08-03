d46fd5a56d5e6197e1ca0df249dec03a3d6c7b62
Stefan Engels <stefan.engels@tum.de>
2023-08-03 09:53:14 +0200

    fix branch and add to license

--- .hooks/update_readme_links.py
+++ .hooks/update_readme_links.py
@@ -6,11 +6,12 @@ current_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "H
 print("Current branch:", current_branch)
 
 
-ubuntu = f"[![Ubuntu C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/ubuntu.yml?label=Ubuntu&logo=ubuntu&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/ubuntu.yml?query=branch%3A{current_branch})\n"
-macos = f"[![macOS C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/macos.yml?label=macOS&logo=apple&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/macos.yml?query=branch%3A{current_branch})\n"
-windows = f"[![Windows C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/windows.yml?label=Windows&logo=windows&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/windows.yml?query=branch%3A{current_branch})\n"
-codeql = f"[![CodeQL](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/codeql-analysis.yml?label=CodeQL&logo=github&style=flat-square?branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/codeql-analysis.yml?query=branch%3A{current_branch})\n"
-codecov = f"[![codecov](https://img.shields.io/codecov/c/github/cda-tum/mtct?label=Coverage&logo=codecov&style=flat-square?branch={current_branch})](https://codecov.io/gh/cda-tum/mtct/tree/{current_branch})\n"
+ubuntu = f"[![Ubuntu C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/ubuntu.yml?label=Ubuntu&logo=ubuntu&style=flat-square&branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/ubuntu.yml?query=branch%3A{current_branch})\n"
+macos = f"[![macOS C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/macos.yml?label=macOS&logo=apple&style=flat-square&branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/macos.yml?query=branch%3A{current_branch})\n"
+windows = f"[![Windows C++](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/windows.yml?label=Windows&logo=windows&style=flat-square&branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/windows.yml?query=branch%3A{current_branch})\n"
+codeql = f"[![CodeQL](https://img.shields.io/github/actions/workflow/status/cda-tum/mtct/codeql-analysis.yml?label=CodeQL&logo=github&style=flat-square&branch={current_branch})](https://github.com/cda-tum/mtct/actions/workflows/codeql-analysis.yml?query=branch%3A{current_branch})\n"
+codecov = f"[![codecov](https://img.shields.io/codecov/c/github/cda-tum/mtct?label=Coverage&logo=codecov&style=flat-square&branch={current_branch})](https://codecov.io/gh/cda-tum/mtct/tree/{current_branch})\n"
+lic = f"[![License](https://img.shields.io/github/license/cda-tum/mtct?label=License&style=flat-square&branch={current_branch})](https://github.com/cda-tum/mtct/blob/{current_branch}/LICENSE)\n"
 
 
 lines = open("README.md").readlines()
@@ -31,5 +32,8 @@ with open("README.md", "w") as f:
         elif codecov[:20] in line and codecov not in line:
             print(f"Replacing {line} with {codecov}")
             f.write(codecov)
+        elif lic[:20] in line and lic not in line:
+            print(f"Replacing {line} with {lic}")
+            f.write(lic)
         else:
             f.write(line)
