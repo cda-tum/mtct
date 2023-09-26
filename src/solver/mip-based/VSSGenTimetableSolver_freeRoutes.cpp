aada625369a7f9205a0afd99720051f0218d7a34
Stefan Engels <stefan.engels@tum.de>
2023-09-26 16:39:05 +0200

    finish stop at boundary for free route, todo bugfix infeasibility

--- src/solver/mip-based/VSSGenTimetableSolver_freeRoutes.cpp
+++ src/solver/mip-based/VSSGenTimetableSolver_freeRoutes.cpp
@@ -672,7 +672,53 @@ void cda_rail::solver::mip_based::VSSGenTimetableSolver::
 
 void cda_rail::solver::mip_based::VSSGenTimetableSolver::
     create_non_discretized_free_routes_only_stop_at_vss_constraints() {
-  throw std::runtime_error("Not implemented yet");
+  // For every breakable edge position exactly b_pos if tight
+  for (size_t i = 0; i < breakable_edges.size(); ++i) {
+    const auto& e            = breakable_edges[i];
+    const auto  vss_number_e = instance.n().max_vss_on_edge(e);
+    const auto& edge         = instance.n().get_edge(e);
+    const auto& edge_name    = "[" + instance.n().get_vertex(edge.source).name +
+                            "," + instance.n().get_vertex(edge.target).name +
+                            "]";
+    const auto& e_len = edge.length;
+    for (size_t vss = 0; vss < vss_number_e; ++vss) {
+      for (size_t tr : instance.trains_on_edge(e, this->fix_routes)) {
+        for (size_t t = train_interval[tr].first + 2;
+             t <= train_interval[tr].second; ++t) {
+          model->addConstr(vars["e_mu"](tr, t - 1, e), GRB_GREATER_EQUAL,
+                           vars["b_pos"](i, vss) -
+                               e_len * (1 - vars["b_tight"](tr, t, i, vss)),
+                           "tight_vss_border_constraint_1_" +
+                               std::to_string(tr) + "_" + std::to_string(t) +
+                               "_" + edge_name + "_" + std::to_string(vss));
+          model->addConstr(vars["e_mu"](tr, t - 1, e), GRB_LESS_EQUAL,
+                           vars["b_pos"](i, vss) +
+                               e_len * vars["b_tight"](tr, t, i, vss),
+                           "tight_vss_border_constraint_2_" +
+                               std::to_string(tr) + "_" + std::to_string(t) +
+                               "_" + edge_name + "_" + std::to_string(vss));
+        }
+      }
+    }
+  }
+
+  // Analog for every edge ending
+  for (size_t e = 0; e < num_edges; ++e) {
+    const auto& edge      = instance.n().get_edge(e);
+    const auto& edge_name = "[" + instance.n().get_vertex(edge.source).name +
+                            "," + instance.n().get_vertex(edge.target).name +
+                            "]";
+    const auto& e_len = edge.length;
+    for (size_t tr : instance.trains_on_edge(e, this->fix_routes)) {
+      for (size_t t = train_interval[tr].first + 2;
+           t <= train_interval[tr].second; ++t) {
+        model->addConstr(vars["e_mu"](tr, t - 1, e), GRB_GREATER_EQUAL,
+                         e_len * vars["e_tight"](tr, t, e),
+                         "tight_ttd_border_constraint_" + std::to_string(tr) +
+                             "_" + std::to_string(t) + "_" + edge_name);
+      }
+    }
+  }
 }
 
 // NOLINTEND(performance-inefficient-string-concatenation)
