61da72f653fc233e59fffcdf1ba5f6bd40e8bdb0
Stefan Engels <stefan.engels@tum.de>
2023-10-04 12:43:45 +0200

    Revert "debug output on failed test"
    This reverts commit 434be6980e895a47d7304d1937a30a01aedb3c0d.

--- test/test_gurobi_vss_gen.cpp
+++ test/test_gurobi_vss_gen.cpp
@@ -44,7 +44,6 @@ TEST(Solver, GurobiVSSGenFixedRoute) {
   cda_rail::solver::mip_based::VSSGenTimetableSolver solver(
       "./example-networks/SimpleStation/");
 
-  auto obj_val =
-      solver.solve(15, true, false, false, false, false, true, -1, true);
+  auto obj_val = solver.solve();
   EXPECT_EQ(obj_val, 1);
 }
