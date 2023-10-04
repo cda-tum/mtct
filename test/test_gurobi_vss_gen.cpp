434be6980e895a47d7304d1937a30a01aedb3c0d
Stefan Engels <stefan.engels@tum.de>
2023-10-04 12:25:08 +0200

    debug output on failed test

--- test/test_gurobi_vss_gen.cpp
+++ test/test_gurobi_vss_gen.cpp
@@ -44,6 +44,7 @@ TEST(Solver, GurobiVSSGenFixedRoute) {
   cda_rail::solver::mip_based::VSSGenTimetableSolver solver(
       "./example-networks/SimpleStation/");
 
-  auto obj_val = solver.solve();
+  auto obj_val =
+      solver.solve(15, true, false, false, false, false, true, -1, true);
   EXPECT_EQ(obj_val, 1);
 }
