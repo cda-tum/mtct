d3c0b6ac2b4a6f638df34c3c41642cffcdf2b2cd
Stefan Engels <stefan.engels@tum.de>
2023-11-03 13:50:44 +0100

    improve coverage for custom exceptions

--- test/test_helper.cpp
+++ test/test_helper.cpp
@@ -195,4 +195,64 @@ TEST(VSSModel, Functions) {
   EXPECT_EQ(cda_rail::vss::functions::max_n_blocks(f3, 0.25), 3);
 }
 
+TEST(Exceptions, Content) {
+  const auto e1 = cda_rail::exceptions::ModelCreationException();
+  EXPECT_EQ(std::string(e1.what()), "Model creation failed.");
+  const auto e2 = cda_rail::exceptions::ModelCreationException("test2");
+  EXPECT_EQ(std::string(e2.what()), "test2");
+
+  const auto e3 = cda_rail::exceptions::ExportException();
+  EXPECT_EQ(std::string(e3.what()), "Export failed.");
+  const auto e4 = cda_rail::exceptions::ExportException("test4");
+  EXPECT_EQ(std::string(e4.what()), "test4");
+
+  const auto e5 = cda_rail::exceptions::ConsistencyException();
+  EXPECT_EQ(std::string(e5.what()), "Consistency check failed.");
+  const auto e6 = cda_rail::exceptions::ConsistencyException("test6");
+  EXPECT_EQ(std::string(e6.what()), "test6");
+
+  const auto e5b = cda_rail::exceptions::InvalidInputException();
+  EXPECT_EQ(std::string(e5b.what()), "Invalid input.");
+  const auto e6b = cda_rail::exceptions::InvalidInputException("test6b");
+  EXPECT_EQ(std::string(e6b.what()), "test6b");
+
+  const auto e7 = cda_rail::exceptions::ImportException();
+  EXPECT_EQ(std::string(e7.what()), "Import failed.");
+  const auto e8 = cda_rail::exceptions::ImportException("test8");
+  EXPECT_EQ(std::string(e8.what()), "Import of test8 failed.");
+
+  const auto e9 = cda_rail::exceptions::VertexNotExistentException();
+  EXPECT_EQ(std::string(e9.what()), "Some vertex specified does not exist.");
+  const auto e10 = cda_rail::exceptions::VertexNotExistentException(10);
+  EXPECT_EQ(std::string(e10.what()), "Vertex with ID 10 does not exist");
+
+  const auto e11 = cda_rail::exceptions::EdgeNotExistentException();
+  EXPECT_EQ(std::string(e11.what()), "Some edge specified does not exist.");
+  const auto e12 = cda_rail::exceptions::EdgeNotExistentException(12);
+  EXPECT_EQ(std::string(e12.what()), "Edge with ID 12 does not exist.");
+  const auto e13 = cda_rail::exceptions::EdgeNotExistentException(12, 13);
+  EXPECT_EQ(std::string(e13.what()),
+            "Edge connecting vertices with IDs 12->13 does not exist.");
+  const auto e14 = cda_rail::exceptions::EdgeNotExistentException("v12", "v14");
+  EXPECT_EQ(std::string(e14.what()),
+            "Edge connecting v12->v14 does not exist.");
+
+  const auto e15 = cda_rail::exceptions::TrainNotExistentException();
+  EXPECT_EQ(std::string(e15.what()), "Some train specified does not exist.");
+  const auto e16 = cda_rail::exceptions::TrainNotExistentException(16);
+  EXPECT_EQ(std::string(e16.what()), "Train with ID 16 does not exist.");
+
+  const auto e17 = cda_rail::exceptions::StationNotExistentException();
+  EXPECT_EQ(std::string(e17.what()), "Some station specified does not exist.");
+  const auto e18 = cda_rail::exceptions::StationNotExistentException("S18");
+  EXPECT_EQ(std::string(e18.what()), "Station S18 does not exist.");
+
+  const auto e19 = cda_rail::exceptions::ScheduleNotExistentException();
+  EXPECT_EQ(std::string(e19.what()), "Some schedule specified does not exist.");
+  const auto e20 = cda_rail::exceptions::ScheduleNotExistentException(20);
+  EXPECT_EQ(std::string(e20.what()), "Schedule with ID 20 does not exist.");
+  const auto e21 = cda_rail::exceptions::ScheduleNotExistentException("S21");
+  EXPECT_EQ(std::string(e21.what()), "Schedule S21 does not exist.");
+}
+
 // NOLINTEND(clang-diagnostic-unused-result)
