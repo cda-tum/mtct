f4aaa1a1b2ed6b184472658483a90d6cdbbb75ef
Stefan Engels <stefan.engels@tum.de>
2024-02-07 17:17:28 +0100

    abstract export/import test

--- test/test_abstract_data_structure.cpp
+++ test/test_abstract_data_structure.cpp
@@ -137,6 +137,19 @@ TEST(GeneralAbstractDataStructure, GeneralTimetable) {
   timetable.add_stop("Train1", "Station1", {60, 120}, {120, 180}, 60);
 
   EXPECT_TRUE(timetable.check_consistency(network));
+  EXPECT_EQ(
+      timetable.get_schedule("Train1").get_stops().at(0).get_station_name(),
+      "Station1");
+  EXPECT_EQ(
+      timetable.get_schedule("Train1").get_stops().at(0).get_begin_range(),
+      (std::pair<int, int>(60, 120)));
+  EXPECT_EQ(timetable.get_schedule("Train1").get_stops().at(0).get_end_range(),
+            (std::pair<int, int>(120, 180)));
+  EXPECT_EQ(timetable.get_schedule("Train1")
+                .get_stops()
+                .at(0)
+                .get_min_stopping_time(),
+            60);
 
   EXPECT_THROW(
       timetable.add_stop("Train1", "Station1", {180, 240}, {240, 300}, 60),
@@ -149,4 +162,91 @@ TEST(GeneralAbstractDataStructure, GeneralTimetable) {
   timetable.remove_stop("Train2", "Station1");
 
   EXPECT_TRUE(timetable.check_consistency(network));
+}
+
+TEST(GeneralAbstractDataStructure, GeneralTimetableExportImport) {
+  Network network("./example-networks/SimpleStation/network/");
+
+  GeneralTimetable<GeneralSchedule<GeneralScheduledStop>> timetable;
+
+  const auto l0 = network.get_vertex_index("l0");
+  const auto r0 = network.get_vertex_index("r0");
+
+  timetable.add_train("Train1", 100, 10, 1, 1, true, {0, 60}, 0, "l0",
+                      {360, 420}, 0, "r0", network);
+  timetable.add_train("Train2", 100, 10, 1, 1, false, {0, 60}, 10, l0,
+                      {400, 460}, 5, r0, network);
+
+  timetable.add_station("Station1");
+  timetable.add_track_to_station("Station1", "g00", "g01", network);
+  timetable.add_track_to_station("Station1", "g01", "g00", network);
+  timetable.add_track_to_station("Station1", "g10", "g11", network);
+  timetable.add_track_to_station("Station1", "g11", "g10", network);
+  timetable.add_stop("Train1", "Station1", {60, 120}, {120, 180}, 60);
+
+  EXPECT_TRUE(timetable.check_consistency(network));
+
+  // Write timetable to directory
+  timetable.export_timetable("./tmp/test-general-timetable/", network);
+
+  GeneralTimetable<GeneralSchedule<GeneralScheduledStop>> timetable_read(
+      "./tmp/test-general-timetable/", network);
+
+  std::filesystem::remove_all("./tmp");
+
+  const auto tr1 = timetable_read.get_train_list().get_train_index("Train1");
+  const auto tr2 = timetable_read.get_train_list().get_train_index("Train2");
+
+  EXPECT_TRUE(timetable_read.check_consistency(network));
+
+  EXPECT_EQ(timetable_read.get_train_list().get_train_index("Train1"), tr1);
+  EXPECT_EQ(timetable_read.get_train_list().get_train_index("Train2"), tr2);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").name, "Train1");
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").length, 100);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").max_speed, 10);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").acceleration,
+            1);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").deceleration,
+            1);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train1").tim, true);
+
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").name, "Train2");
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").length, 100);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").max_speed, 10);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").acceleration,
+            1);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").deceleration,
+            1);
+  EXPECT_EQ(timetable_read.get_train_list().get_train("Train2").tim, false);
+
+  EXPECT_EQ(timetable_read.get_schedule(tr1).get_t_0_range(),
+            (std::pair<int, int>(0, 60)));
+  EXPECT_EQ(timetable_read.get_schedule(tr1).get_t_n_range(),
+            (std::pair<int, int>(360, 420)));
+  EXPECT_EQ(timetable_read.get_schedule(tr1).get_v_0(), 0);
+  EXPECT_EQ(timetable_read.get_schedule(tr1).get_v_n(), 0);
+
+  EXPECT_EQ(timetable_read.get_schedule("Train2").get_t_0_range(),
+            (std::pair<int, int>(0, 60)));
+  EXPECT_EQ(timetable_read.get_schedule("Train2").get_t_n_range(),
+            (std::pair<int, int>(400, 460)));
+  EXPECT_EQ(timetable_read.get_schedule("Train2").get_v_0(), 10);
+  EXPECT_EQ(timetable_read.get_schedule("Train2").get_v_n(), 5);
+
+  EXPECT_EQ(timetable_read.get_schedule("Train1")
+                .get_stops()
+                .at(0)
+                .get_station_name(),
+            "Station1");
+  EXPECT_EQ(
+      timetable_read.get_schedule("Train1").get_stops().at(0).get_begin_range(),
+      (std::pair<int, int>(60, 120)));
+  EXPECT_EQ(
+      timetable_read.get_schedule("Train1").get_stops().at(0).get_end_range(),
+      (std::pair<int, int>(120, 180)));
+  EXPECT_EQ(timetable_read.get_schedule("Train1")
+                .get_stops()
+                .at(0)
+                .get_min_stopping_time(),
+            60);
 }
