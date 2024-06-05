32f4636c77f2816375020060f3859c79c662f79b
Stefan Engels <stefan.engels@tum.de>
2024-06-05 18:43:39 +0200

    header file for a* solver

---
+++ include/solver/a-star-based/AStarVSSPerformanceOptimizationSolver.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "probleminstances/GeneralPerformanceOptimizationInstance.hpp"
+#include "solver/GeneralSolver.hpp"
+
+#include <filesystem>
+#include <string>
+#include <utility>
+
+namespace cda_rail::solver::astar_based {
+class AStarVSSPerformanceOptimizationSolver
+    : public GeneralSolver<
+          instances::GeneralPerformanceOptimizationInstance,
+          instances::SolGeneralPerformanceOptimizationInstance<
+              instances::GeneralPerformanceOptimizationInstance>> {
+private:
+  // TODO: Implement
+
+public:
+  // Constructors. TODO: Implement
+  explicit AStarVSSPerformanceOptimizationSolver(
+      const instances::GeneralPerformanceOptimizationInstance& instance);
+  explicit AStarVSSPerformanceOptimizationSolver(
+      const std::filesystem::path& p);
+  explicit AStarVSSPerformanceOptimizationSolver(const std::string& path);
+  explicit AStarVSSPerformanceOptimizationSolver(const char* path);
+
+  // TODO: Implement missing functions
+
+  using GeneralSolver::solve;
+  [[nodiscard]] instances::SolGeneralPerformanceOptimizationInstance<
+      instances::GeneralPerformanceOptimizationInstance>
+  solve(int time_limit, bool debug_input) override;
+};
+} // namespace cda_rail::solver::astar_based
