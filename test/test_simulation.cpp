8614ccb6844836216cf789226f86a65ff8ef6345
Severin Lochschmidt <seviron53@gmail.com>
2024-11-27 18:27:30 +0100

    Add example test for new simulation category

---
+++ test/test_simulation.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Log.h>
+#include <plog/Logger.h>
+#include <string>
+
+TEST(Simulation, Example) {}
