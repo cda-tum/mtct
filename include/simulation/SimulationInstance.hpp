132c990ea8cb698ad5d46c74b9443a433e77ac54
Severin Lochschmidt <seviron53@gmail.com>
2024-11-27 19:22:37 +0100

    Add empty class `SimulationInstance`

---
+++ include/simulation/SimulationInstance.hpp
@@ -0,0 +1,11 @@
+#include "datastructure/RailwayNetwork.hpp"
+#include "probleminstances/GeneralProblemInstance.hpp"
+
+namespace cda_rail {
+
+class SimulationInstance : public instances::GeneralProblemInstance {
+  /**
+   * Environment for Microscopic Simulation
+   */
+}
+}; // namespace cda_rail
