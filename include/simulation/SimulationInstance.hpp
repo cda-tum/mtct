de8214fe70357389401683ed9dcb665b2677f08d
Severin Lochschmidt <seviron53@gmail.com>
2024-11-28 12:14:15 +0100

    Add members and test for `SimulationInstance`

--- include/simulation/SimulationInstance.hpp
+++ include/simulation/SimulationInstance.hpp
@@ -1,11 +1,26 @@
 #include "datastructure/RailwayNetwork.hpp"
-#include "probleminstances/GeneralProblemInstance.hpp"
+#include "datastructure/Timetable.hpp"
 
 namespace cda_rail {
 
-class SimulationInstance : public instances::GeneralProblemInstance {
+struct SimulationParameters {
+  u_int64_t n_timesteps;
+  u_int64_t n_v_target_vars;
+};
+
+class SimulationInstance {
   /**
    * Environment for Microscopic Simulation
    */
-}
+
+  const Network              network;
+  const Timetable            timetable;
+  const SimulationParameters parameters;
+
+public:
+  SimulationInstance(Network network, Timetable timetable,
+                     SimulationParameters parameters)
+      : network(network), timetable(timetable), parameters(parameters) {};
+};
+
 }; // namespace cda_rail
--- test/test_simulation.cpp
+++ test/test_simulation.cpp
@@ -1,11 +1,20 @@
+#include "datastructure/RailwayNetwork.hpp"
+#include "datastructure/Timetable.hpp"
+#include "simulation/SimulationInstance.hpp"
+
+using namespace cda_rail;
+
 #include "gtest/gtest.h"
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <plog/Formatters/TxtFormatter.h>
-#include <plog/Initializers/RollingFileInitializer.h>
 #include <plog/Log.h>
 #include <plog/Logger.h>
-#include <string>
 
-TEST(Simulation, Example) {}
+TEST(Simulation, SimulationInstance) {
+  Network network =
+      Network::import_network("./example-networks/SimpleStation/network/");
+  Timetable timetable = Timetable::import_timetable(
+      "./example-networks/SimpleStation/timetable/", network);
+  SimulationParameters parameters = {.n_timesteps = 200, .n_v_target_vars = 20};
+
+  const SimulationInstance instance =
+      SimulationInstance(network, timetable, parameters);
+}
