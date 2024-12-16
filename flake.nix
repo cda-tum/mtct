f351e64a02ffdd967044313627371d3523d04db8
Severin Lochschmidt <seviron53@gmail.com>
2024-12-16 15:47:34 +0100

    Update dev packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "MTCT DevShell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = inputs@{ flake-parts, ... }:
