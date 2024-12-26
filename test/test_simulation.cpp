74c35ef9388965a4bf40bef133263e878e082b05
Severin Lochschmidt <seviron53@gmail.com>
2024-12-26 14:47:09 +0100

    Add test for trajectory construction

--- test/test_simulation.cpp
+++ test/test_simulation.cpp
@@ -53,3 +53,26 @@ TEST(Simulation, EdgeTrajectory) {
     EdgeEntry      transition = edge_traj.enter_next_edge(0.3);
   }
 }
+
+TEST(Simulation, TrainTrajectory) {
+  const ulong seed =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  std::ranlux24_base rng_engine(seed);
+  Network            network =
+      Network::import_network("./example-networks/SimpleStation/network/");
+  Timetable timetable = Timetable::import_timetable(
+      "./example-networks/SimpleStation/timetable/", network);
+
+  SimulationInstance                   instance(network, timetable, 20);
+  std::uniform_int_distribution<ulong> random_train_index(
+      0, timetable.get_train_list().size() - 1);
+
+  for (int i = 0; i < 1000; i++) {
+    Train train =
+        timetable.get_train_list().get_train(random_train_index(rng_engine));
+
+    RoutingSolution solution(10, 10, instance.n_timesteps, train, rng_engine);
+
+    TrainTrajectory traj(instance, train, solution);
+  }
+}
