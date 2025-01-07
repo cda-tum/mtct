5d29b7a0aae2a0e096489f64c191afe54a8bb090
Severin Lochschmidt <seviron53@gmail.com>
2025-01-07 16:31:56 +0100

    Disable unused and cpp warnings
    fortify warning cannot be turned off otherwise even with explicit `D_FORTIFY_SOURCE=0`

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -40,8 +40,9 @@ function(enable_project_options target_name)
                                                     -fno-trapping-math>)
 
     # enable some more options for better debugging
-    target_compile_options(${target_name} INTERFACE $<$<CONFIG:DEBUG>:-O0 -fno-omit-frame-pointer
-                                                    -fno-optimize-sibling-calls -fno-inline-functions>)
+    target_compile_options(
+      ${target_name} INTERFACE $<$<CONFIG:DEBUG>:-O0 -Wno-cpp -Wno-unused-result -fno-omit-frame-pointer
+                               -fno-optimize-sibling-calls -fno-inline-functions>)
   endif()
 
   option(BINDINGS "Configure for building Python bindings")
