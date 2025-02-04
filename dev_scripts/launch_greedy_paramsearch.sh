94f9cf285fb6c3bdd3159cdf919a4692ac02eeee
Severin Lochschmidt <seviron53@gmail.com>
2025-02-04 17:11:17 +0100

    Update dev scripts for launching apps

---
+++ dev_scripts/launch_greedy_paramsearch.sh
@@ -0,0 +1,1 @@
+./build/apps/rail_vss_generation_timetable_simulator_greedy_parameter_testing ./test/example-networks-unidirec/SimpleNetwork .
--- dev_scripts/launch_measure.sh
+++
@@ -1,1 +0,0 @@
-./build/apps/rail_vss_generation_timetable_simulator_measurement ./test/example-networks-unidirec/SimpleNetwork .
