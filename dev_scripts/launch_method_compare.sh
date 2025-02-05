b9c0824faa2df1e77afd8f68addcc823e54a5e4c
Severin Lochschmidt <seviron53@gmail.com>
2025-02-05 15:36:52 +0100

    Measure on same network

--- dev_scripts/launch_method_compare.sh
+++ dev_scripts/launch_method_compare.sh
@@ -1,1 +1,1 @@
-./build/apps/rail_vss_generation_timetable_simulator_search_methods_testing ./test/example-networks-unidirec/Overtake .
+./build/apps/rail_vss_generation_timetable_simulator_search_methods_testing ./test/example-networks-unidirec/SimpleNetwork .
