a66f91bec9a60e3b382e0106d085fc93be080086
Stefan Engels <stefan.engels@tum.de>
2025-06-13 17:59:43 +0200

    test basic functions

--- test/test_greedysimulator.cpp
+++ test/test_greedysimulator.cpp
@@ -132,3 +132,94 @@ TEST(GreedySimulator, CheckConsistency) {
   EXPECT_FALSE(simulator12.check_consistency());
   EXPECT_FALSE(simulator_instance2.check_consistency());
 }
+
+TEST(GreedySimulator, BasicFunctions) {
+  Network     network("./example-networks/SimpleStation/network/");
+  const auto& ttd_sections = network.unbreakable_sections();
+  const auto& l0_l1        = network.get_edge_index("l0", "l1");
+  const auto& l1_l2        = network.get_edge_index("l1", "l2");
+  const auto& l2_l3        = network.get_edge_index("l2", "l3");
+  const auto& r0_r1        = network.get_edge_index("r0", "r1");
+  const auto& r1_r2        = network.get_edge_index("r1", "r2");
+
+  GeneralTimetable<GeneralSchedule<GeneralScheduledStop>> timetable;
+  const auto l0  = network.get_vertex_index("l0");
+  const auto r0  = network.get_vertex_index("r0");
+  const auto tr1 = timetable.add_train("Train1", 100, 10, 1, 1, true, {0, 60},
+                                       0, "l0", {360, 420}, 0, "r0", network);
+  const auto tr2 = timetable.add_train("Train2", 100, 10, 1, 1, false, {0, 60},
+                                       10, r0, {400, 460}, 5, l0, network);
+  timetable.add_station("Station1");
+  timetable.add_track_to_station("Station1", "g00", "g01", network);
+  timetable.add_track_to_station("Station1", "g01", "g00", network);
+  timetable.add_track_to_station("Station1", "g10", "g11", network);
+  timetable.add_track_to_station("Station1", "g11", "g10", network);
+  timetable.add_stop("Train1", "Station1", {60, 120}, {120, 180}, 60);
+  EXPECT_TRUE(timetable.check_consistency(network));
+
+  RouteMap routes;
+
+  cda_rail::instances::GeneralPerformanceOptimizationInstance instance(
+      network, timetable, routes);
+  EXPECT_TRUE(instance.check_consistency(false));
+
+  cda_rail::instances::GeneralPerformanceOptimizationInstance instance2(
+      network, timetable, routes);
+  instance2.add_empty_route("Train1");
+  instance2.push_back_edge_to_route("Train1", "l0", "l1");
+  EXPECT_FALSE(instance2.check_consistency(false));
+
+  cda_rail::simulator::GreedySimulator simulator(instance, ttd_sections);
+  EXPECT_THROW(simulator.set_train_edges({{l0_l1}}),
+               cda_rail::exceptions::InvalidInputException);
+  simulator.set_train_edges({{l0_l1, l1_l2, l2_l3}, {r0_r1, r1_r2}});
+  const auto& tr_edges1 = simulator.get_train_edges();
+  EXPECT_EQ(tr_edges1.size(), 2);
+  EXPECT_EQ(tr_edges1[0].size(), 3);
+  EXPECT_EQ(tr_edges1[1].size(), 2);
+  EXPECT_EQ(tr_edges1[0][0], l0_l1);
+  EXPECT_EQ(tr_edges1[0][1], l1_l2);
+  EXPECT_EQ(tr_edges1[0][2], l2_l3);
+  EXPECT_EQ(tr_edges1[1][0], r0_r1);
+  EXPECT_EQ(tr_edges1[1][1], r1_r2);
+  simulator.set_train_edges_of_tr(0, {l0_l1, l1_l2});
+  const auto& tr_edges2 = simulator.get_train_edges_of_tr(0);
+  EXPECT_EQ(tr_edges2.size(), 2);
+  EXPECT_EQ(tr_edges2[0], l0_l1);
+  EXPECT_EQ(tr_edges2[1], l1_l2);
+  EXPECT_THROW(simulator.get_train_edges_of_tr(1000),
+               cda_rail::exceptions::TrainNotExistentException);
+
+  EXPECT_THROW(simulator.set_ttd_orders({}),
+               cda_rail::exceptions::InvalidInputException);
+  simulator.set_ttd_orders(std::vector<std::vector<size_t>>(
+      ttd_sections.size(), std::vector<size_t>()));
+  const auto& ttd_orders1 = simulator.get_ttd_orders();
+  EXPECT_EQ(ttd_orders1.size(), ttd_sections.size());
+  for (const auto& orders : ttd_orders1) {
+    EXPECT_TRUE(orders.empty());
+  }
+  simulator.set_ttd_orders_of_ttd(0, {tr1, tr2});
+  const auto& ttd_orders2 = simulator.get_ttd_orders_of_ttd(0);
+  EXPECT_EQ(ttd_orders2.size(), 2);
+  EXPECT_EQ(ttd_orders2[0], tr1);
+  EXPECT_EQ(ttd_orders2[1], tr2);
+  EXPECT_THROW(simulator.get_ttd_orders_of_ttd(1000),
+               cda_rail::exceptions::InvalidInputException);
+
+  EXPECT_THROW(simulator.set_entry_orders({}),
+               cda_rail::exceptions::InvalidInputException);
+  simulator.set_entry_orders(std::vector<std::vector<size_t>>(
+      network.number_of_vertices(), std::vector<size_t>()));
+  const auto& entry_orders1 = simulator.get_entry_orders();
+  EXPECT_EQ(entry_orders1.size(), network.number_of_vertices());
+  for (const auto& orders : entry_orders1) {
+    EXPECT_TRUE(orders.empty());
+  }
+  simulator.set_entry_orders_of_vertex(l0, {tr1});
+  const auto& entry_orders2 = simulator.get_entry_orders_of_vertex(l0);
+  EXPECT_EQ(entry_orders2.size(), 1);
+  EXPECT_EQ(entry_orders2[0], tr1);
+  EXPECT_THROW(simulator.get_entry_orders_of_vertex(1000),
+               cda_rail::exceptions::InvalidInputException);
+}
