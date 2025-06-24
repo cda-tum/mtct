0b8fb3f51c0568160b1244a96b7494999b743857
Stefan Engels <stefan.engels@tum.de>
2025-06-24 14:37:03 +0200

    Revert "test coverage of ma and maxv function"
    This reverts commit 94d0dce3d5c3d455ca03430473dc345fd092bab0.

--- test/test_greedysimulator.cpp
+++ test/test_greedysimulator.cpp
@@ -17,7 +17,6 @@ using namespace cda_rail;
 // NOLINTBEGIN
 // (clang-analyzer-deadcode.DeadStores,misc-const-correctness,clang-diagnostic-unused-result)
 
-/**
 TEST(GreedySimulator, CheckConsistency) {
   // Create instance
   Network     network("./example-networks/SimpleStation/network/");
@@ -1810,8 +1809,6 @@ TEST(GreedySimulator, ExitHeadwaySpeedConstraint) {
   EXPECT_THROW(simulator.get_max_speed_exit_headway(tr1, train1, 455, 5, 8, -1),
                cda_rail::exceptions::InvalidInputException);
 }
-
- **/
 
 TEST(GreedySimulator, MAandMaxV) {
   Network    network;
