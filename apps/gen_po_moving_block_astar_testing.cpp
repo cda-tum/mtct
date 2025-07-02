359dd08550426301ffc692cfd6a5c3fb348e7d80
Stefan Engels <stefan.engels@tum.de>
2025-07-02 14:39:34 +0200

    app for astar solver

---
+++ apps/gen_po_moving_block_astar_testing.cpp
@@ -0,0 +1,89 @@
+#include "Definitions.hpp"
+#include "plog/Init.h"
+#include "plog/Logger.h"
+#include "plog/Severity.h"
+#include "solver/astar-based/GenPOMovingBlockAStarSolver.hpp"
+
+#include <cstdlib>
+#include <gsl/span>
+#include <plog/Appenders/ColorConsoleAppender.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Log.h>
+#include <string>
+
+// NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-pro-bounds-array-to-pointer-decay,bugprone-exception-escape)
+
+int main(int argc, char** argv) {
+  // Only log to console using std::cerr and std::cout respectively unless
+  // initialized differently
+  if (plog::get() == nullptr) {
+    static plog::ColorConsoleAppender<plog::TxtFormatter> console_appender;
+    plog::init(plog::debug, &console_appender);
+  }
+
+  if (argc != 10) {
+    PLOGE << "Expected 9 arguments, got " << argc - 1;
+    std::exit(-1);
+  }
+
+  auto              args          = gsl::span<char*>(argv, argc);
+  const std::string model_name    = args[1];
+  const std::string instance_path = args[2];
+  auto              solver =
+      cda_rail::solver::astar_based::GenPOMovingBlockAStarSolver(instance_path);
+
+  const int  dt                           = std::stoi(args[3]);
+  const bool ensure_feasibility           = std::stoi(args[4]) != 0;
+  const bool limit_speed_by_leaving_edges = std::stoi(args[5]) != 0;
+  const int  next_state_strategy_int      = std::stoi(args[6]);
+  const auto next_state_strategy =
+      static_cast<cda_rail::solver::astar_based::NextStateStrategy>(
+          next_state_strategy_int);
+  const int  remaining_time_heuristic_int = std::stoi(args[7]);
+  const auto remaining_time_heuristic =
+      static_cast<cda_rail::simulator::RemainingTimeHeuristicType>(
+          remaining_time_heuristic_int);
+  const bool consider_earliest_exit = std::stoi(args[8]) != 0;
+
+  const int timeout = std::stoi(args[9]);
+
+  PLOGI << "The following parameters were passed:";
+  PLOGI << "Model name: " << model_name;
+  PLOGI << "Instance path: " << instance_path;
+  PLOGI << "Time step (dt): " << dt;
+  PLOGI << "Ensure feasibility: " << (ensure_feasibility ? "yes" : "no");
+  PLOGI << "Limit speed by leaving edges: "
+        << (limit_speed_by_leaving_edges ? "yes" : "no");
+  switch (next_state_strategy) {
+  case cda_rail::solver::astar_based::NextStateStrategy::SingleEdge:
+    PLOGI << "Next state strategy: SingleEdge";
+    break;
+  case cda_rail::solver::astar_based::NextStateStrategy::NextTTD:
+    PLOGI << "Next state strategy: NextTTD";
+    break;
+  }
+  switch (remaining_time_heuristic) {
+  case cda_rail::simulator::RemainingTimeHeuristicType::Zero:
+    PLOGI << "Remaining time heuristic: Zero";
+    break;
+  case cda_rail::simulator::RemainingTimeHeuristicType::Simple:
+    PLOGI << "Remaining time heuristic: Simple";
+    break;
+  }
+  PLOGI << "Consider earliest exit: "
+        << (consider_earliest_exit ? "yes" : "no");
+  PLOGI << "Timeout: " << timeout;
+
+  // NOLINTNEXTLINE(clang-diagnostic-unused-result)
+  solver.solve({.dt                           = dt,
+                .late_entry_possible          = ensure_feasibility,
+                .late_exit_possible           = ensure_feasibility,
+                .late_stop_possible           = ensure_feasibility,
+                .limit_speed_by_leaving_edges = limit_speed_by_leaving_edges},
+               {.remaining_time_heuristic_type = remaining_time_heuristic,
+                .next_state_strategy           = next_state_strategy,
+                .consider_earliest_exit        = consider_earliest_exit},
+               {.name = model_name}, timeout, true, true);
+}
+
+// NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-pro-bounds-array-to-pointer-decay,bugprone-exception-escape)
