9175460b109fffa20a45d27d0cac2c8ca7e769b7
Stefan Engels <stefan.engels@tum.de>
2026-03-27 15:41:58 +0100

    generate docstring for Train class

--- include/datastructure/Train.hpp
+++ include/datastructure/Train.hpp
@@ -12,12 +12,15 @@
 namespace cda_rail {
 class Train {
   /**
-   * Train struct with relevant properties
-   * @param name Name of the train
-   * @param length Length of the train (in m)
-   * @param max_speed Maximum speed of the train (in m/s)
-   * @param acceleration Acceleration of the train (in m/s^2)
-   * @param deceleration Deceleration of the train (in m/s^2)
+   * @brief Properties and characteristics of a train.
+   *
+   * Stores the name, length, maximum speed, and acceleration/deceleration
+   * capabilities, as well as train integrity monitoring status.
+   *
+   * @invariant m_length >= 0 (non-negative).
+   * @invariant m_max_speed >= MIN_NON_ZERO (strictly positive).
+   * @invariant m_acceleration >= MIN_NON_ZERO (strictly positive).
+   * @invariant m_deceleration >= MIN_NON_ZERO (strictly positive).
    */
 private:
   std::string m_name;         // name of train
@@ -29,7 +32,22 @@ private:
                     // default true
 
 public:
-  // Constructor
+  /**
+   * @brief Constructs a train with specified dynamic properties.
+   *
+   * @pre length >= 0.
+   * @pre max_speed >= MIN_NON_ZERO.
+   * @pre acceleration >= MIN_NON_ZERO.
+   * @pre deceleration >= MIN_NON_ZERO.
+   * @param name Name identifier for the train.
+   * @param length Length of the train in meters.
+   * @param max_speed Maximum speed of the train in m/s.
+   * @param acceleration Maximum acceleration capability in m/s².
+   * @param deceleration Maximum deceleration capability in m/s².
+   * @param tim Train integrity monitoring flag (default: true).
+   * @throws cda_rail::exceptions::InvalidInputException If any numeric
+   * precondition is violated.
+   */
   Train(std::string name, double length, double max_speed, double acceleration,
         double deceleration, bool tim = true);
   // Rule of 0 suffices
@@ -37,38 +55,128 @@ public:
   /*
    * GETTER
    */
+  /**
+   * @brief Returns the name of the train.
+   *
+   * @return Train name.
+   */
   [[nodiscard]] std::string get_name() const { return m_name; };
-  [[nodiscard]] double      get_length() const { return m_length; };
-  [[nodiscard]] double      get_max_speed() const { return m_max_speed; };
-  [[nodiscard]] double      get_acceleration() const { return m_acceleration; };
-  [[nodiscard]] double      get_deceleration() const { return m_deceleration; };
-  [[nodiscard]] bool        has_tim() const { return m_tim; };
+
+  /**
+   * @brief Returns the length of the train.
+   *
+   * @return Train length in meters (guaranteed >= 0).
+   */
+  [[nodiscard]] double get_length() const { return m_length; };
+
+  /**
+   * @brief Returns the maximum speed of the train.
+   *
+   * @return Maximum speed in m/s (guaranteed >= MIN_NON_ZERO).
+   */
+  [[nodiscard]] double get_max_speed() const { return m_max_speed; };
+
+  /**
+   * @brief Returns the maximum acceleration of the train.
+   *
+   * @return Maximum acceleration in m/s² (guaranteed >= MIN_NON_ZERO).
+   */
+  [[nodiscard]] double get_acceleration() const { return m_acceleration; };
+
+  /**
+   * @brief Returns the maximum deceleration of the train.
+   *
+   * @return Maximum deceleration in m/s² (guaranteed >= MIN_NON_ZERO).
+   */
+  [[nodiscard]] double get_deceleration() const { return m_deceleration; };
+
+  /**
+   * @brief Returns whether the train has train integrity monitoring.
+   *
+   * @return `true` if train has TIM enabled, `false` otherwise.
+   */
+  [[nodiscard]] bool has_tim() const { return m_tim; };
 
   /*
    * SETTER
    */
+  /**
+   * @brief Sets the name of the train.
+   *
+   * @param name New name for the train.
+   */
   void set_name(std::string name) { m_name = std::move(name); };
+
+  /**
+   * @brief Sets the length of the train.
+   *
+   * @pre length >= 0.
+   * @param length New length in meters.
+   * @throws cda_rail::exceptions::InvalidInputException If `length` is
+   * negative.
+   */
   void set_length(double const length) {
     cda_rail::exceptions::throw_if_negative(length, "Train length");
     m_length = length;
   };
+
+  /**
+   * @brief Sets the maximum speed of the train.
+   *
+   * @pre max_speed >= MIN_NON_ZERO.
+   * @param max_speed New maximum speed in m/s.
+   * @throws cda_rail::exceptions::InvalidInputException If `max_speed` is less
+   * than MIN_NON_ZERO.
+   */
   void set_max_speed(double const max_speed) {
     cda_rail::exceptions::throw_if_less_than(max_speed, MIN_NON_ZERO,
                                              "Train max speed");
     m_max_speed = max_speed;
   };
+
+  /**
+   * @brief Sets the maximum acceleration of the train.
+   *
+   * @pre acceleration >= MIN_NON_ZERO.
+   * @param acceleration New maximum acceleration in m/s².
+   * @throws cda_rail::exceptions::InvalidInputException If `acceleration` is
+   * less than MIN_NON_ZERO.
+   */
   void set_acceleration(double const acceleration) {
     cda_rail::exceptions::throw_if_less_than(acceleration, MIN_NON_ZERO,
                                              "Train acceleration");
     m_acceleration = acceleration;
   }
+
+  /**
+   * @brief Sets the maximum deceleration of the train.
+   *
+   * @pre deceleration >= MIN_NON_ZERO.
+   * @param deceleration New maximum deceleration in m/s².
+   * @throws cda_rail::exceptions::InvalidInputException If `deceleration` is
+   * less than MIN_NON_ZERO.
+   */
   void set_deceleration(double const deceleration) {
     cda_rail::exceptions::throw_if_less_than(deceleration, MIN_NON_ZERO,
                                              "Train deceleration");
     m_deceleration = deceleration;
   };
+
+  /**
+   * @brief Sets the train integrity monitoring flag to a specific value.
+   *
+   * @param tim `true` to enable TIM, `false` to disable.
+   */
   void set_tim_value(bool const tim) { m_tim = tim; };
+
+  /**
+   * @brief Enables train integrity monitoring.
+   */
   void set_tim() { set_tim_value(true); };
+
+  /**
+   * @brief Disables train integrity monitoring.
+   */
   void set_no_tim() { set_tim_value(false); };
 };
 
