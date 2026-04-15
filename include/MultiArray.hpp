3b685186a06acb7a5c89cd8b2575376f155ae58a
Stefan Engels <stefan.engels@tum.de>
2026-04-15 16:48:39 +0200

    separating into cpp does not work this way

--- include/MultiArray.hpp
+++ include/MultiArray.hpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <memory>
+#include <sstream>
 #include <stdexcept>
 #include <utility>
 #include <vector>
@@ -50,4 +51,42 @@ public:
   [[nodiscard]] size_t size() const { return m_data.size(); }
   [[nodiscard]] size_t dimensions() const { return m_shape.size(); }
 };
+
+template <typename T>
+void MultiArray<T>::check_args(const FixedSizeVector<size_t>& args) const {
+  if (m_shape.size() != args.size()) {
+    throw std::invalid_argument(
+        "Number of dimensions and number of arguments do not coincide.");
+  }
+  for (size_t i = 0; i < args.size(); ++i) {
+    if (args[i] >= m_shape[i]) {
+      std::stringstream ss;
+      ss << "Index " << args[i] << " is too large for dimension " << i;
+      throw std::out_of_range(ss.str());
+    }
+  }
+}
+
+template <typename T>
+size_t MultiArray<T>::flat_index(const FixedSizeVector<size_t>& args) const {
+  check_args(args);
+  size_t index      = 0;
+  size_t multiplier = 1;
+  for (size_t i = 0; i < args.size(); ++i) {
+    index += args[i] * multiplier;
+    multiplier *= m_shape[i];
+  }
+  return index;
+}
+
+template <typename T>
+void MultiArray<T>::init(const FixedSizeVector<size_t>& dims) {
+  m_shape    = dims;
+  size_t cap = 1;
+  for (const auto& d : dims) {
+    cap *= d;
+  }
+  m_data.delete_and_resize(cap);
+}
+
 } // namespace cda_rail
