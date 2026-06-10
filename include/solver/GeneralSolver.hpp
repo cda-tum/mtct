e51dd2783664f67afee6796dee92a251303782f3
Stefan Engels <stefan.engels@tum.de>
2026-06-10 17:22:14 +0200

    add general solver

--- include/solver/GeneralSolver.hpp
+++ include/solver/GeneralSolver.hpp
@@ -33,36 +33,36 @@ template <typename T, typename S> class GeneralSolver {
       "S must be a child of SolGeneralProblemInstance<T>");
 
 protected:
-  T                                                   instance;
-  decltype(std::chrono::high_resolution_clock::now()) start;
-  decltype(std::chrono::high_resolution_clock::now()) model_created;
-  decltype(std::chrono::high_resolution_clock::now()) model_solved;
-  int64_t                                             create_time = 0;
-  int64_t                                             solve_time  = 0;
+  T                                                   m_instance;
+  decltype(std::chrono::high_resolution_clock::now()) m_start;
+  decltype(std::chrono::high_resolution_clock::now()) m_model_created;
+  decltype(std::chrono::high_resolution_clock::now()) m_model_solved;
+  int64_t                                             m_create_time = 0;
+  int64_t                                             m_solve_time  = 0;
 
   void solve_init_general(int time_limit, bool debug_input,
                           bool overwrite_severity) {
     cda_rail::initialize_plog(debug_input, overwrite_severity);
 
     if (plog::get()->checkSeverity(plog::debug) || time_limit > 0) {
-      start = std::chrono::high_resolution_clock::now();
+      m_start = std::chrono::high_resolution_clock::now();
     }
   }
 
   GeneralSolver() = default;
-  explicit GeneralSolver(const T& instance) : instance(instance) {};
-  explicit GeneralSolver(const std::filesystem::path& p) : instance(p) {};
-  explicit GeneralSolver(const std::string& path) : instance(path) {};
-  explicit GeneralSolver(const char* path) : instance(path) {};
+  explicit GeneralSolver(const T& instance) : m_instance(instance) {};
+  explicit GeneralSolver(std::string_view const instanceName,
+                         std::string_view const instanceSubdirectory)
+      : m_instance(instanceName, instanceSubdirectory) {};
 
 public:
-  [[nodiscard]] const T& get_instance() const { return instance; }
-  [[nodiscard]] T&       editable_instance() { return instance; }
+  [[nodiscard]] const T& get_instance() const { return m_instance; }
+  [[nodiscard]] T&       editable_instance() { return m_instance; }
 
   [[nodiscard]] S solve() { return solve(-1, false); };
   [[nodiscard]] S solve(int time_limit, bool debug_input) {
     return solve(time_limit, debug_input, true);
-  };
+  }
   [[nodiscard]] virtual S solve(int time_limit, bool debug_input,
                                 bool overwrite_severity) = 0;
 
